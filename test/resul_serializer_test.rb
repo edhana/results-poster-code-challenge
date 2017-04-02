c86404cf71e6ae775d593e3f5babb7e6862573bd
Eduardo Santos Marques <eduardo.marques@avixy.com>
2017-04-02 16:43:16 -0300

    Added an externat fixtures file to help the tests

--- test/resul_serializer_test.rb
+++ test/resul_serializer_test.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require_relative 'fixtures.rb'
 
 class BigFiveResultTextSerializer
   def initialize(test_result_str)
@@ -40,6 +41,7 @@ class BigFiveResultTextSerializer
       end
     end
 
+    # TODO: Refactor repeated code
     sub_hash['facets'] = facets_hash
     result_hash[header_line[0]] = sub_hash
 
@@ -49,32 +51,7 @@ end
 
 describe BigFiveResultTextSerializer do
   before do
-    @basic_text = <<-RESULT
-Extraversion
-
-Extraversion is marked by pronounced engagement with the external world. Extraverts enjoy being with people, are full of energy, and often experience positive emotions. They tend to be enthusiastic, action-oriented, individuals who are likely to say "Yes!" or "Let's go!" to opportunities for excitement. In groups they like to talk, assert themselves, and draw attention to themselves.
-
-Introverts lack the exuberance, energy, and activity levels of extraverts. They tend to be quiet, low-key, deliberate, and disengaged from the social world. Their lack of social involvement should not be interpreted as shyness or depression; the introvert simply needs less stimulation than an extravert and prefers to be alone. The independence and reserve of the introvert is sometimes mistaken as unfriendliness or arrogance. In reality, an introvert who scores high on the agreeableness dimension will not seek others out but will be quite pleasant when approached.
-
-
-Domain/Facet............ Score
-
-EXTRAVERSION...............87
-
-..Friendliness.............94
-
-..Gregariousness...........80
-
-..Assertiveness............81
-
-..Activity Level...........88
-
-..Excitement-Seeking.......46
-
-..Cheerfulness.............66
-
-Your score on Extraversion is high, indicating you are sociable, outgoing, energetic, and lively. You prefer to be around people much of the time.
-    RESULT
+    @basic_text = get_text
 
     @serializer =  BigFiveResultTextSerializer.new @basic_text
   end
