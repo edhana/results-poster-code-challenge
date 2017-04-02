2e64438399f9367b186c57eab553e8a54f747878
Eduardo Santos Marques <eduardo.marques@avixy.com>
2017-04-02 19:36:47 -0300

    Refactored the results_poster post function (made it functional)

--- results_poster.rb
+++ results_poster.rb
@@ -1,4 +1,5 @@
 require 'faraday'
+require 'json'
 
 class BigFiveResultsPoster
   attr_reader :response_code, :token
@@ -10,6 +11,29 @@ class BigFiveResultsPoster
   end
 
   def post
-    return false
+    conn = Faraday.new(:url => 'https://recruitbot.trikeapps.com') do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
+
+    response = conn.post do |req|
+      req.url 'https://recruitbot.trikeapps.com/api/v1/roles/mid-senior-web-developer/big_five_profile_submissions'
+      req.headers['Content-Type'] = 'application/json'
+      req.body = @results.to_json
+    end
+
+    @response_code = response.status
+
+    if response.status == 201
+      @token = response.body
+    elsif response.status == 422
+      # print the errors into STDOUT
+      puts "RESPONSE ERRORS:"
+      puts "#{response.body}"
+      return false
+    end
+
+    return true
   end
 end
--- test/result_post_test.rb
+++ test/result_post_test.rb
@@ -2,31 +2,28 @@ require 'minitest/autorun'
 require 'json'
 require 'results_serializer.rb'
 require 'results_poster.rb'
-require 'faraday'
 require_relative 'fixtures.rb'
 
 describe BigFiveResultsPoster do
   before do
     @serializer =  BigFiveResultTextSerializer.new get_text
-    @results_poster = BigFiveResultsPoster.new @serializer.to_hash
+  end
+
+  it "should fail with code 422 with an invalid hash" do
+    poster = BigFiveResultsPoster.new({'test' => 'random value'})
+    poster.post.must_equal false
+    poster.token.must_be_nil
+    poster.response_code.must_be :==, 422
   end
 
   it "should post a random json to the bot" do
-    #@results_poster.post.must_equal true
-    conn = Faraday.new(:url => 'https://recruitbot.trikeapps.com/api/v1/roles/mid-senior-web-developer/big_five_profile_submissions') do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
-
-    response = conn.post do |req|
-      req.url 'https://recruitbot.trikeapps.com/api/v1/roles/mid-senior-web-developer/big_five_profile_submissions'
-      req.headers['Content-Type'] = 'application/json'
-      #req.body = '{ "name": "Unagi" }'
-      puts "====>>>> JSON BODY: #{ @serializer.to_hash.to_json}"
-      req.body = @serializer.to_hash.to_json
-    end
-
-    puts "=====>>> RESPONSE: #{response.body} "
+    r = Random.new
+    result_hash = @serializer.to_hash
+    result_hash['NAME'] = "Test man"
+    result_hash['EMAIL'] = "testman#{r.rand(10.99)}@test.com"
+    results_poster = BigFiveResultsPoster.new result_hash
+    results_poster.post.must_equal true
+    results_poster.token.wont_be_nil
+    results_poster.response_code.must_be :==, 201
   end
 end
